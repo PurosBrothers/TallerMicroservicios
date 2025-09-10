b461515cf9296ce4d18310bc09059267f6b1675f
joshuaPZ <joshuaprieto8@gmail.com>
2025-09-10 14:21:58 -0500

    .

---
+++ punto1/tallerMicroservicios/src/test/java/com/taller/tallerMicroservicios/TallerMicroserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.tallerMicroservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerMicroserviciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
