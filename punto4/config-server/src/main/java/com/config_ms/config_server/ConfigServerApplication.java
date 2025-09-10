e1a257f2929b5357c0d2d9b52b580804d3686ea7
ahenaon00 <ahenao.2005@gmail.com>
2025-09-10 14:57:23 -0500

    added config for config server

--- punto4/config-server/src/main/java/com/config_ms/config_server/ConfigServerApplication.java
+++ punto4/config-server/src/main/java/com/config_ms/config_server/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.config_ms.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
